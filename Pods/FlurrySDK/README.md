b21ac374fcbcc95ca868ab77d03b3777516a6589
Christian Hatch <christianhatch@gmail.com>
2014-07-09 04:45:24 -0400

    added pods, updated readme

---
+++ Pods/FlurrySDK/README.md
@@ -0,0 +1,18 @@
+Flurry
+======
+
+Latest Flurry SDK
+
+To use 5.0.0 FlurrySDK from cocoapods, add this line in your Pods file:
+
+```
+  pod 'FlurrySDK', '5.0'
+```
+
+
+If you also want the FlurryAds SDK, you can define pods to install both subspecs:
+
+```
+  pod 'FlurrySDK/FlurrySDK', '5.0'
+  pod 'FlurrySDK/FlurryAds', '5.0'
+```
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// FlurrySDK
+#define COCOAPODS_POD_AVAILABLE_FlurrySDK
+#define COCOAPODS_VERSION_MAJOR_FlurrySDK 5
+#define COCOAPODS_VERSION_MINOR_FlurrySDK 2
+#define COCOAPODS_VERSION_PATCH_FlurrySDK 0
+
+// FlurrySDK/FlurrySDK
+#define COCOAPODS_POD_AVAILABLE_FlurrySDK_FlurrySDK
+#define COCOAPODS_VERSION_MAJOR_FlurrySDK_FlurrySDK 5
+#define COCOAPODS_VERSION_MINOR_FlurrySDK_FlurrySDK 2
+#define COCOAPODS_VERSION_PATCH_FlurrySDK_FlurrySDK 0
+
