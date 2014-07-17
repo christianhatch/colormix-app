be4f52cef1094da7d100bac97e699ad88fdeea36
Christian Hatch <christianhatch@gmail.com>
2014-07-17 17:51:44 -0400

    removed constants, added a tools .h file

--- Colormix/ColormixKit.h
+++ Colormix/ColormixKit.h
@@ -1,22 +1,13 @@
 //
-//  ColormixConstants.h
+//  ColormixKit
 //  Colormix
 //
-//  Created by Christian Hatch on 7/9/14.
+//  Created by Christian Hatch on 7/17/14.
 //  Copyright (c) 2014 Commodoreftp. All rights reserved.
 //
 
 
-#pragma mark - Constants
-
-#define FlurryAppID @"353K59ZD8JXYTKDZPYRQ"
-
-
-
-
-
 #pragma mark - Macros
-
 
 #ifdef DEBUG
 #define DebugLog( s, ... ) NSLog(@"%@:(%d) %@", [NSString stringWithUTF8String:__PRETTY_FUNCTION__], __LINE__, [NSString stringWithFormat:(s), __VA_ARGS__])
