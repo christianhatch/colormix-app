0b931365b9aa94c67604c0ef724e95d2afd87957
Christian Hatch <christianhatch@gmail.com>
2015-04-19 20:56:07 -0400

    removed autorotation and updated for iphone 6/+

---
+++ Colormix/Common/GradientSlider.h
@@ -0,0 +1,13 @@
+//
+//  GradientSlider.h
+//  Colormix
+//
+//  Created by Christian Hatch on 4/19/15.
+//  Copyright (c) 2015 Commodoreftp. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GradientSlider : UISlider
+
+@end
